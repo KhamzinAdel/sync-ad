1fe0564fea7f59599f8f3e3ea5f6464f3bfba57c
KhamzinAdel <khamzin.adel@mail.ru>
2025-04-27 13:45:05 +0300

    оставил открытие на каждое соединение

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import logging
+from logger import configure_logging
+from services import OrganizationUnitService, ActiveDirectoryService
+
+
+from entities.schemas import OrganizationUnitADSchema
+
+logger = logging.getLogger(__name__)
+
+
+def sync_organizations_with_ad() -> None:
+    """Вызов функции создания OU и групп"""
+
+    organization_service: OrganizationUnitService = OrganizationUnitService()
+    active_directory_service: ActiveDirectoryService = ActiveDirectoryService()
+
+    organizations = organization_service.get_ou_to_active_directory()
+
+    for organization in organizations:
+        active_directory_service.create_uo_and_group(
+            ou_name=organization.name,
+            ou_path=organization.ou_path,
+            base_code=organization.base_code,
+        )
+
+    logger.info('Организации cинхронизированы с AD')
+
+
+if __name__ == '__main__':
+    configure_logging()
+    sync_organizations_with_ad()
