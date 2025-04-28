421e48aacd49445700bfe9bb424df338a60e5c31
KhamzinAdel <khamzin.adel@mail.ru>
2025-04-28 20:26:21 +0300

    добавил логи

--- src/infrastructure/logger.py
+++ src/infrastructure/logger.py
@@ -2,8 +2,14 @@ import logging
 
 
 def configure_logging(level=logging.INFO):
+    handlers = [
+        logging.StreamHandler(),
+        logging.FileHandler("sync_ad.log")
+    ]
+
     logging.basicConfig(
         level=level,
         datefmt="%Y-%m-%d %H:%M:%S",
         format="[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s",
+        handlers=handlers,
     )
