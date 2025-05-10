778b3acb7b5aa53d1c74a0bc55628beaf7eebfcd
KhamzinAdel <khamzin.adel@mail.ru>
2025-05-10 22:26:14 +0300

    рефакторинг кода

--- src/utils/ou_builder.py
+++ src/utils/ou_builder.py
@@ -29,12 +29,7 @@ class OUBuilder:
             resolved_parent_name = GROUP_NAME.get(parent_name, parent_name)
             full_path = f'OU={resolved_parent_name},' + full_path
 
-        # надо потом удалить просто return full_path
-        f = ','.join(full_path.split(',')[:-3]) + ',' + 'dc=stud,dc=local'
-        if f in FULL_PATH_AD:
-            resolved_parent_name = GROUP_NAME.get(parent_name, parent_name)
-            f = f'OU={resolved_parent_name},' + f
-        return f
+        return full_path
 
     @classmethod
     def truncate_name(cls, name: str, max_length: int = 64) -> str:
